6310c078cfdbd690a807e3b84656491bd615180b
deeptidewani014 <techiedeeptipadhy0722@gmail.com>
2022-05-18 09:27:02 +0530

    hi

---
+++ Screens/Hi.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Hi() {
+    return (
+        <View>
+            <Text>Hi</Text>
+        </View>
+    )
+}
