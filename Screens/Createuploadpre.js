a447d858539dc9327383b68e0eb651646d684864
deeptidewani014 <techiedeeptipadhy0722@gmail.com>
2022-05-18 09:11:15 +0530

    upload

---
+++ Screens/Createuploadpre.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Createuploadpre = () => {
+    return (
+        <View>
+            <Text>Createuploadpre</Text>
+        </View>
+    )
+}
+
+export default Createuploadpre
+
+const styles = StyleSheet.create({})
